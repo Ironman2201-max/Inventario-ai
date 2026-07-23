a3892ddbf694a4bddd1f090d487a45a204ceee47
Ironman2201-max <jose113292322@gmail.com>
2026-07-23 01:29:12 -0500

    Remove ContainerHistory route from app.routes
    Removed the route for the ContainerHistory component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { GestionUsuarios } from './pages/gestion-usuarios/gestion-usuarios';
 import { ContainerRegister } from './modules/containers/container-register/container-register';
 import { ContainerList } from './modules/containers/container-list/container-list';
 import { ContainerExit } from './modules/containers/container-exit/container-exit';
-import { ContainerHistory } from './modules/containers/container-history/container-history';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -38,9 +37,7 @@ export const routes: Routes = [
  { path: 'contenedores/registro', component: ContainerRegister },
  { path: 'contenedores/salida', component: ContainerExit },
    
- { 
-    path: 'containers/history', component: ContainerHistory 
-  },
+ 
 
     { path: 'contenedores/inventario', component: ContainerList },
   // Redirección por defecto si la ruta no existe
