a1538596724d43f4a786dafe634876836d2cc2e3
Ironman2201-max <jose113292322@gmail.com>
2026-07-23 12:29:17 -0500

    agregando la nueva atulizacion ruta  de histrila de salida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { GestionUsuarios } from './pages/gestion-usuarios/gestion-usuarios';
 import { ContainerRegister } from './modules/containers/container-register/container-register';
 import { ContainerList } from './modules/containers/container-list/container-list';
 import { ContainerExit } from './modules/containers/container-exit/container-exit';
-
+import { ContainerHistory } from './modules/containers/container-history/container-history';
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'registro', component: Registro },
@@ -36,7 +36,10 @@ export const routes: Routes = [
   },
  { path: 'contenedores/registro', component: ContainerRegister },
  { path: 'contenedores/salida', component: ContainerExit },
- { path: 'contenedores/historial', component: ContainerList },
+  { path: 'contenedores/inventario', component: ContainerList },
+ { path: 'contenedores/historial', component: ContainerHistory },
+
+     
   // Redirección por defecto si la ruta no existe
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
