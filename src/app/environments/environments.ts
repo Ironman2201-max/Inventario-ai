3e514d08c59d64b5b1339efad2b8987c5f749954
Ironman2201-max <jose113292322@gmail.com>
2026-07-25 21:32:13 -0500

    Update API URL configuration to switch to local backend and comment out previous production URL

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,7 +3,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://162-35-169-164.sslip.io/api'
-   //apiUrl : 'http://localhost/angular-backend'
+ //piUrl: 'https://162-35-169-164.sslip.io/api'
+ apiUrl : 'http://localhost/angular-backend'
 
 };
